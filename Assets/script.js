a836066a4562f6dab6470b9d5163e9deff003fe7
paradxgh34 <137127578+paradxgh34@users.noreply.github.com>
2023-09-06 18:55:40 -0400

    created JS script for addtional TODO's

--- Assets/script.js
+++ Assets/script.js
@@ -14,16 +14,41 @@ timeBlock.on('click', '.saveBtn', function() {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+  timeBlock
+    if (parseInt($(this).attr('id')) < dayjs().format('H')) {
+      $(this).addClass('past')
+    } else if (parseInt($(this).attr('id')) > dayjs().format('H')) {
+      $(this).addClass('future')
+    } else {
+      $(this).addClass('present') 
+    } 
+  })
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+  for (i = 9; i < timeBlock.length + 9; i++) {
+    var input = "#" + i
+    var realInput = i.toString()
+    if (localStorage.getItem(realInput + ' boxText') != null) {
+      $(input).find('.description').val(localStorage.getItem(realInput + ' boxText'))
+    }
+  }
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+window.onload = setInterval(function() {
+  var currentDay = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  $('#currentDay').text(currentDay)
+}, 0)
+
+  
   // TODO: Add code to display the current date in the header of the page.
 });
